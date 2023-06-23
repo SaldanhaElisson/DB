7d8cd758fda7e7e2eb4916a016c377b1e886246f
SaldanhaElisson <elissonsaldanha19@gmail.com>
2023-06-23 16:05:07 -0300

    add cliente

---
+++ src/main/java/br/com/frota/model/Cliente.java
@@ -0,0 +1,36 @@
+package br.com.frota.model;
+
+public class Cliente extends GenericModel{
+    private String numDocumento;
+    private String numCliente;
+    private  Pessoa pessoa;
+
+    public Cliente(Integer id, String num_documento, String num_cliente, Pessoa pessoa) {
+        this.numDocumento = num_documento;
+        this.numCliente = num_cliente;
+        this.pessoa = pessoa;
+        super.setId(id);
+    }
+
+    public Integer getIdPessoa() {
+        return pessoa.getId();
+    }
+
+    @Override
+    public String toString() {
+        return "tarefa_rota { \n" +
+                "\t id='" + this.getId() + "\' \n"  +
+                "\t num_documento =" + numDocumento + "\n" +
+                "\t num_cliente = " + numCliente + "\n" +
+                "\t pessoa ="+ pessoa + "\n" +
+                "\t } ";
+    }
+
+    public String getNumDocumento() {
+        return numDocumento;
+    }
+
+    public String getNumCliente() {
+        return numCliente;
+    }
+}
